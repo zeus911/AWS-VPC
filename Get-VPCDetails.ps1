2d8c85d7b31b9234820e359dc0f689ecb8c7aa56
achaplin <ajc@kintford.com.au>
2014-11-11 15:01:01 +0000

    Added Get-VPCDetails.ps1 Deleted various versions of Create-MultiTier-VPC(LAB)#.template Updated Create-MultiTier-VPC(LAB)5.template

---
+++ Get-VPCDetails.ps1
@@ -0,0 +1,25 @@
+﻿
+$VPCID = "vpc-f50bca90"
+
+
+
+
+# VPC
+Get-EC2VPC -
+
+# Subnet
+
+# RouteTable
+
+# Internet Gateway
+
+# DHCP Options Set
+
+# Elastic IPs
+
+# VPC Peering Connections
+
+# Network ACL
+
+# Security Group
+
